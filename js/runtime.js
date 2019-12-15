f18aa5f98d6e68a29766b50393cbfbf3e3a3de70
Samuel Sosa <samuelsosaaleman@gmail.com>
2019-12-15 00:58:02 -0600

    modificaciones interfaz y runtime

--- js/runtime.js
+++ js/runtime.js
@@ -1,20 +1,16 @@
-var lista=[];
-var lista1=[];
-var t1,t2,t3,t4, tburbuja, tquicksort;
-var sortedArray ;
+var lista = [];
+var lista1 = [];
+var t1, t2, t3, t4, tburbuja, tquicksort;
+var sortedArray;
 
 
-for (let index = 0; index < 50000; index++) {
-    lista[index]=Math.floor(Math.random() * 100);
-    lista1[index]=Math.floor(Math.random() * 100);
 
-    
-}
+
 function Burbuja() {
 
     var n, i, k, aux;
     n = lista.length;
-  //  console.log(lista); // Mostramos, por consola, la lista desordenada
+    //  console.log(lista); // Mostramos, por consola, la lista desordenada
     // Algoritmo de burbuja
     for (k = 1; k < n; k++) {
         for (i = 0; i < (n - k); i++) {
@@ -33,39 +29,86 @@ function Burbuja() {
 //quicksort
 
 function quick_Sort(origArray) {
-	if (origArray.length <= 1) { 
-		return origArray;
-	} else {
+    if (origArray.length <= 1) {
+        return origArray;
+    } else {
 
-		var left = [];
-		var right = [];
-		var newArray = [];
-		var pivot = origArray.pop();
-		var length = origArray.length;
+        var left = [];
+        var right = [];
+        var newArray = [];
+        var pivot = origArray.pop();
+        var length = origArray.length;
 
-		for (var i = 0; i < length; i++) {
-			if (origArray[i] <= pivot) {
-				left.push(origArray[i]);
-			} else {
-				right.push(origArray[i]);
-			}
-		}
-		return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
+        for (var i = 0; i < length; i++) {
+            if (origArray[i] <= pivot) {
+                left.push(origArray[i]);
+            } else {
+                right.push(origArray[i]);
+            }
+        }
+        return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
     }
-    
+
 }
 
-t3=performance.now();
-sortedArray = quick_Sort(lista);
-t4=performance.now();
+function ordenar() {
+    lista = [];
+    for (let index = 0; index < document.getElementById('cantidad').value; index++) {
+        lista[index] = Math.floor(Math.random() * 100);
+        lista1[index] = Math.floor(Math.random() * 100);
 
-t1=performance.now();
-Burbuja(lista1);
-t2=performance.now();
 
-tburbuja=t2-t1;
-tquicksort=t4-t3;
-console.log(sortedArray);
-console.log(tburbuja+'ms  '+tquicksort+'ms');
-document.writeln('tiempo de ejecucion de con burbuja '+tburbuja/1000+'s  ');
-document.writeln('tiempo de ejecucion de con burbuja '+tquicksort/1000+'s');
+    }
+    t3 = performance.now();
+    sortedArray = quick_Sort(lista);
+    t4 = performance.now();
+
+    t1 = performance.now();
+    Burbuja(lista1);
+    t2 = performance.now();
+
+    tburbuja = t2 - t1;
+    tquicksort = t4 - t3;
+    var tiempos = [];
+    tiempos[0] = tburbuja / 1000;
+    tiempos[1] = tquicksort / 1000;
+    graficar(tiempos);
+
+}
+
+
+/*console.log(sortedArray);
+console.log(tburbuja + 'ms  ' + tquicksort + 'ms');
+document.writeln('tiempo de ejecucion de con burbuja ' + tburbuja / 1000 + 's  ');
+document.writeln('tiempo de ejecucion de con burbuja ' + tquicksort / 1000 + 's');*/
+
+function graficar(datos) {
+
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+
+        type: 'bar',
+
+
+        data: {
+            labels: [document.getElementById('cantidad').value, document.getElementById('cantidad').value],
+            datasets: [{
+                label: 'Tiempos de ejecucion',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: datos
+            }]
+        },
+
+        options: {}
+    });
+
+    //pintar en arreglo vertical
+    if (!container.hasChildNodes) {
+        container.forEach(element => {
+            list.removeChild(element);
+        });
+    }
+
+}
--- js/shakersort.js
+++ js/shakersort.js
@@ -1,8 +1,8 @@
 "use strict";
- 
+
 var datos = [];
 var backgroundColor = [];
-var comparando ;
+var comparando;
 var segundos;
 var cantidad;
 var container = document.getElementById('arreglo');
@@ -20,18 +20,18 @@ function ordenar() {
     //document.getElementById('arreglo').innerHTML = ``;
     document.getElementById('shaker').innerHTML = ``;
     comparando = 'rgba(252, 20, 20)';
-    segundos = 3;//document.getElementById('segundos').value;
-    cantidad = 5;//document.getElementById('cantidad').value;
+    segundos = 3; //document.getElementById('segundos').value;
+    cantidad = 5; //document.getElementById('cantidad').value;
     //document.getElementById('listaAleatoria').innerHTML = ``;
     for (let i = 0; i < cantidad; i++) {
         datos[i] = Math.floor(Math.random() * 100);
         backgroundColor[i] = 'rgba(255, 99, 132, 0.2)';
-   //     document.getElementById('listaAleatoria').innerHTML += `
-     //  <div class="col colorAleatoria">${datos[i]}</div>
-       // `;
+        //     document.getElementById('listaAleatoria').innerHTML += `
+        //  <div class="col colorAleatoria">${datos[i]}</div>
+        // `;
 
     }
- 
+
     for (let i = 0; i < datos.length; i++) {
         var objeto = document.createElement("div");
 
@@ -51,63 +51,61 @@ function ordenar() {
 
 
 
-async function shaker(arr){
-let ordenando = true;
-graficar(arr);
-await timer(2*1000);
-while (ordenando){
-   
-    
-    for (let i = 0; i< arr.length - 1;i++){
+async function shaker(arr) {
+    let ordenando = true;
+    graficar(arr);
+    await timer(2 * 1000);
+    while (ordenando) {
+
+
+        for (let i = 0; i < arr.length - 1; i++) {
             backgroundColor[i] = 'rgba(20, 248, 252)';
-            backgroundColor[i+1] =  'rgba(20, 248, 252)';
+            backgroundColor[i + 1] = 'rgba(20, 248, 252)';
             graficar(arr);
             await timer(2 * 1000);
-            if (arr[i] > arr[i + 1])
-             {
+            if (arr[i] > arr[i + 1]) {
 
-               
+
                 let temp = arr[i];
                 arr[i] = arr[i + 1];
-                arr[i+1] = temp;
+                arr[i + 1] = temp;
                 ordenando = true;
-              
+
                 console.log(arr);
-              
-             }
-             backgroundColor[i] = 'rgba(255, 99, 132, 0.2)';
-    }
- 
-    if (!ordenando)
-        break;
- 
-    ordenando = false;
- 
-    for (let j = arr.length - 1; j > 0; j--){
-      
-        backgroundColor[j-1]=comparando;
-        backgroundColor[j] = comparando;
-        graficar(arr);
-        
-        await timer(2 * 1000);
-            if (arr[j-1] > arr[j])
-             {
-               
+
+            }
+            backgroundColor[i] = 'rgba(255, 99, 132, 0.2)';
+        }
+
+        if (!ordenando)
+            break;
+
+        ordenando = false;
+
+        for (let j = arr.length - 1; j > 0; j--) {
+
+            backgroundColor[j - 1] = comparando;
+            backgroundColor[j] = comparando;
+            graficar(arr);
+
+            await timer(2 * 1000);
+            if (arr[j - 1] > arr[j]) {
+
 
                 let temp = arr[j];
                 arr[j] = arr[j - 1];
                 arr[j - 1] = temp;
                 ordenando = true;
-                
+
                 console.log(arr);
-             }
-             
-             backgroundColor[j] = 'rgba(255, 99, 132, 0.2)';
+            }
+
+            backgroundColor[j] = 'rgba(255, 99, 132, 0.2)';
+        }
+
     }
-    
-}
-console.log(arr);
- 
+    console.log(arr);
+
 }
 
 
@@ -119,7 +117,7 @@ function graficar(datos) {
     var chart = new Chart(ctx, {
 
         type: 'bar',
-        easing:'',
+        easing: '',
 
         data: {
             labels: datos,
